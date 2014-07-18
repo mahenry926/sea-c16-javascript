9527ae69460297eec059d45f1c69db8ca9a91cd3
Matthew Henry <mahenry@Matthew-Henrys-MacBook-Pro.local>
2014-07-18 14:47:21 -0700

    Adding stringDelimiter.js

---
+++ students/matt-henry/stringDelimeter.js
@@ -0,0 +1,23 @@
+// Parses a string and creates an array of strings as separated by a defined separator
+var stringCount = function stringDelimiter( str, string_separator ) {
+	var parsed_array = [];
+	var string_place = 0;
+	var tempString = '';
+
+	while ( string_place < str.length ) {
+		if (str[string_place] === string_separator) {
+			parsed_array.push( tempString ); 
+			tempString = '';
+		} else {
+			tempString += str[string_place];
+		}
+		string_place++;
+	}
+	return parsed_array;
+}
+
+var testString = 'this$is$a$fun$example$';
+console.log( 'Test string: ' + testString);
+var parsedResults = stringCount( testString, '$');
+console.log( 'Parsed results: ');
+console.log(parsedResults);
