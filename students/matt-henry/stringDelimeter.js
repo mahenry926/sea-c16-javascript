88cea4fc01d945700c99ab109cc35fbaf463b05d
Matthew Henry <mahenry@Matthew-Henrys-MacBook-Pro.local>
2014-07-21 18:12:26 -0700

    Fixing missing semicolon

--- students/matt-henry/stringDelimeter.js
+++ students/matt-henry/stringDelimeter.js
@@ -1,23 +1,21 @@
 // Parses a string and creates an array of strings as separated by a defined separator
-var stringCount = function stringDelimiter( str, string_separator ) {
-	var parsed_array = [];
-	var string_place = 0;
-	var tempString = '';
+var stringCount = function stringDelimiter(str, string_separator) {
+        var parsed_array = [], string_place = 0, tempString = '';
 
-	while ( string_place < str.length ) {
-		if (str[string_place] === string_separator) {
-			parsed_array.push( tempString ); 
-			tempString = '';
-		} else {
-			tempString += str[string_place];
-		}
-		string_place++;
-	}
-	return parsed_array;
-}
+        while (string_place < str.length) {
+                if (str[string_place] === string_separator) {
+                        parsed_array.push(tempString);
+                        tempString = '';
+                } else {
+                        tempString += str[string_place];
+                }
+                string_place++;
+        }
+        return parsed_array;
+};
 
 var testString = 'this$is$a$fun$example$';
-console.log( 'Test string: ' + testString);
-var parsedResults = stringCount( testString, '$');
-console.log( 'Parsed results: ');
+console.log('Test string: ' + testString);
+var parsedResults = stringCount(testString, '$');
+console.log('Parsed results: ');
 console.log(parsedResults);
