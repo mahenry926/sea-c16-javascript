160ab7fd4ba1889934e10c99763cac62cc3c6e54
Matthew Henry <mahenry@Matthew-Henrys-MacBook-Pro.local>
2014-07-28 11:40:51 -0700

    updating stringDelimeter for a few edge cases/examples

--- students/matt-henry/stringDelimeter.js
+++ students/matt-henry/stringDelimeter.js
@@ -4,16 +4,45 @@ var stringCount = function stringDelimiter(str, string_separator) {
 
         while (string_place < str.length) {
                 if (str[string_place] === string_separator) {
-                        parsed_array.push(tempString);
-                        tempString = '';
+			if ( string_place != 0 ) {
+                        	parsed_array.push(tempString);
+                        	tempString = '';
+			}
                 } else {
                         tempString += str[string_place];
                 }
                 string_place++;
         }
+
+	if ( tempString != '' ) {	
+	        parsed_array.push(tempString);
+        	tempString = '';
+	}	
         return parsed_array;
 };
 
+console.log('ENDS WITH NO $');
+var testString = 'this$example$doesnt$end$with$a$dollar$sign';
+console.log('Test string: ' + testString);
+var parsedResults = stringCount(testString, '$');
+console.log('Parsed results: ');
+console.log(parsedResults);
+
+console.log('BEGINS WITH $');
+var testString = '$money$starts$with$dollar$signs$';
+console.log('Test string: ' + testString);
+var parsedResults = stringCount(testString, '$');
+console.log('Parsed results: ');
+console.log(parsedResults);
+
+console.log('BUNCHES OF $$$');
+var testString = 'this$$example$has$empty$$$strings$';
+console.log('Test string: ' + testString);
+var parsedResults = stringCount(testString, '$');
+console.log('Parsed results: ');
+console.log(parsedResults);
+
+console.log('ASSIGNMENT EXAMPLE');
 var testString = 'this$is$a$fun$example$';
 console.log('Test string: ' + testString);
 var parsedResults = stringCount(testString, '$');
